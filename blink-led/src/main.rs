4a298fb7edf0d5b768188cd77af6a4e206e3003d
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-01-25 05:38:00 +0000

    add little code

--- blink-led/src/main.rs
+++ blink-led/src/main.rs
@@ -4,15 +4,24 @@
 use panic_halt as _;
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::hprintln;
+
+use stm32f0::stm32f0x2::Peripherals;
 
 #[entry]
 fn main() -> ! {
-    let test = 3;
+    hprintln!("Hello, world!").unwrap();
 
-    hprintln!("Hello, world! {0}", test).unwrap();
+    let peripherals = Peripherals::take().unwrap();
+    let gpioa = peripherals.GPIOA;
+
     // exit QEMU
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
-    debug::exit(debug::EXIT_SUCCESS);
-    loop {}
+    // debug::exit(debug::EXIT_SUCCESS);
+    let reader = gpioa.odr.read();
+
+    loop {
+        reader.odr0().bit();
+        gpioa.odr.write(|writer| writer.odr0().set_bit());
+    }
 }
